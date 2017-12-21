bc817befcd2335e915060b408402126ca1b7f228
Cookie Solomon <shoshsolomon0@gmail.com>
2017-12-21 09:39:22 +0200

    changes

--- main.js
+++ main.js
@@ -1,43 +1,29 @@
-//INDIVIDUAL EXERCISE 1
+
 
 var reservations = {
   'Bob': { claimed: false },
+
   'Ted': { claimed: true }
-}
-function keysToLowerCase(obj) {
-  var keys = Object.keys(obj);
-  var n = keys.length;
-  while (n--) {
-    var key = keys[n]; // "cache" it, for less lookups to the array
-    if (key !== key.toLowerCase()) { // might already be in its lower case version
-      obj[key.toLowerCase()] = obj[key] // swap the value to a new lower case key
-      delete obj[key] // delete the old key
-    }
-  }
-  return (obj);
-}
-keysToLowerCase(reservations);
+};
+
 
 var claimReservation = function () {
   var name = document.getElementById("pers").value;
-
-  name = name.toLowerCase();
-
-
-  //if person has a reservation
-  if (reservations[name]) {
-    //if their table is claimed 
-    if (reservations[name].claimed) {
-      alert("sorry it was claimed!");
-    } else {
-      alert('Welcome');
+  for (var input in reservations) {
+    if (name.toLowerCase() == input.toLowerCase() && reservations[input].claimed === false) {
+      alert("Welcome!");
+      document.getElementById('confirmed').innerHTML = "Your table is ready " + name;
+      return;
+    } else if (name.toLowerCase() == input.toLowerCase() && reservations[input].claimed === true) {
+      alert("your friends are waiting");
+      document.getElementById('taken').innerHTML = "Table has been claimed for " + name;
+      return;
     }
-  }
-  // if the person hasnt made a res...add to reservations array
-  else {
-    reservations[name] = { claimed: true };
-    document.getElementById('output').innerHTML = "New Reservation made for " + name;
-  }
-}
 
-//claimReservation();
+  }
+
+  reservations[name] = { claimed: true };
+  alert("We've added your reservation");
+  document.getElementById('new-res').innerHTML = "New Reservation made for " + name;
+  return;
+};
